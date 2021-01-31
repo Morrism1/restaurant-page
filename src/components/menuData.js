18845a7d7ad79d68d67f3b399f2697e9645703db
Morrism1 <morrismontana0@gmail.com>
2021-01-31 05:36:18 +0200

    Add restaurant menu data

---
+++ src/components/menuData.js
@@ -0,0 +1,40 @@
+const menuData = [
+  {
+    name: 'Boiled Beef Sauce',
+    Image:
+      'https://res.cloudinary.com/morrism1/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1611847068/pexels-sebastian-coman-photography-3659862_kkbkgi.jpg',
+    price: '$ 20.00',
+  },
+  {
+    name: 'Lunch Galore',
+    Image:
+      'https://res.cloudinary.com/morrism1/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1611847059/lily-banse--YHSwy6uqvk-unsplash_zd6qsf.jpg',
+    price: '$ 60.00',
+  },
+  {
+    name: 'Vegetable Salad',
+    Image:
+      'https://res.cloudinary.com/morrism1/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1611847058/anna-pelzer-IGfIGP5ONV0-unsplash_iptpwn.jpg',
+    price: '$ 14.20',
+  },
+  {
+    name: 'Fruit Salad',
+    Image:
+      'https://res.cloudinary.com/morrism1/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1611847047/jannis-brandt-8manzosDSGM-unsplash_jo39up.jpg',
+    price: '$ 10.00',
+  },
+  {
+    name: 'Marine Love',
+    Image:
+      'https://res.cloudinary.com/morrism1/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1611847036/pexels-christel-jensen-628776_y2weq8.jpg',
+    price: '$ 25.00',
+  },
+  {
+    name: 'Noodle Green Sauce',
+    Image:
+      'https://res.cloudinary.com/morrism1/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1611847033/pexels-daria-shevtsova-1070053_sbhrdp.jpg',
+    price: '$ 30.00',
+  },
+];
+
+export default menuData;
